68cf8b7979e225e74891160219892ddc2e931c66
Daniel Lauzon <daniel.lauzon@irdeto.com>
2018-12-15 18:10:22 -0500

    Get a site logo: failed

--- doczrc.js
+++ doczrc.js
@@ -2,6 +2,12 @@ export default {
   title: 'Team Logos',
   description: 'Team Logos',
   themeConfig: {
-    // mode: 'dark'
+    // mode: 'dark',
+    // logo: {
+    //   // src: '/static/img/Helium.sans-256.c5fb77e0.png',
+    //   src: '/static/img/Helium.sans.1a14d255.svg',
+    //   // src: '/static/img/XRobot.full.c145a71d.svg',
+    //   width: 32
+    // }
   }
 }
