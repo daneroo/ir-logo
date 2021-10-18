b74a2bf3cf2d1f04ef2c9451621994377118f91c
Daniel Lauzon <daniel.lauzon@irdeto.com>
2021-10-18 19:17:05 -0400

    Adjusted Iris SVG Logo Component

--- packages/site/src/pages/index.js
+++ packages/site/src/pages/index.js
@@ -2,36 +2,30 @@ import React from 'react'
 import clsx from 'clsx'
 import Layout from '@theme/Layout'
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
+import useBaseUrl from '@docusaurus/useBaseUrl'
 import styles from './index.module.css'
 import HomepageFeatures from '../components/HomepageFeatures'
 import { Iris } from '../components/logos/iris'
+import { irdetoPurple, irdetoGreen } from '../../src/components/logos/colors'
 
 function HomepageHeader () {
   const { siteConfig } = useDocusaurusContext()
   return (
-    <header
-      className={clsx('hero hero--primary', styles.heroBanner)}
-      // can't figure out how to get bg image url in css module, so here it is as a style prop
-      // https://css-tricks.com/tinted-images-multiple-backgrounds/
-      style={{
-        // backgroundImage: `url(img/Iris/Iris_versicolor_3.jpeg)`,
-        // backgroundImage: `url(img/Iris/Iris_versicolor_Quebec_2-crop-right.jpeg)`,
-        // backgroundImage: `url(img/Iris/Iris_versicolor_Quebec_2-crop-left.jpeg)`,
-        // backgroundImage: `url(img/Iris/Iris_versicolor_Quebec_2.jpeg)`,
-        // backgroundImage: `linear-gradient(to right, rgba(0,0,0, 0.0),rgba(0,0,0, 0.6),rgba(0,0,0, 0.0)),url(img/Iris/Iris_versicolor_Quebec_2.jpeg)`,
-        // backgroundImage: `linear-gradient(to right, rgba(0,0,0, 0.0),rgba(0,0,0, 0.6),rgba(0,0,0, 0.3)),url(img/Iris/Iris_versicolor_Quebec_2-crop-left.jpeg)`,
-        backgroundImage: 'linear-gradient(to right, rgba(128,128,128, .5),rgba(0,0,0, 0.6),rgba(0,0,0, 0.0)),url(img/Iris/Iris_versicolor_Quebec_2-crop-right.jpeg)'
-
-      }}
-    >
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className={clsx('container', styles.containerFlex)}>
-        <div className='left'>
+        <div>
           <Iris size={200} />
-          {/* <img style={{width:'200px'}} src={useBaseUrl('img/Iris/iris-versicolor-logo-400.svg')} alt="Xrobot" /> */}
         </div>
-        <div className='right'>
-          <h1 className='hero__title'>{siteConfig.title}</h1>
-          <p className='hero__subtitle'>{siteConfig.tagline}</p>
+        <div>
+          <h1 className="hero__title">{siteConfig.title}</h1>
+          <p className="hero__subtitle">{siteConfig.tagline}</p>
+        </div>
+        <div className="hero__imageright">
+          <img
+            style={{ width: '300px' }}
+            src={useBaseUrl('img/Iris/Iris_versicolor_3-alpha.png')}
+            alt="Xrobot"
+          />
         </div>
       </div>
     </header>
@@ -43,8 +37,7 @@ export default function Home () {
   return (
     <Layout
       title={siteConfig.title}
-      description='Description will go into a meta tag in <head />'
-    >
+      description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
